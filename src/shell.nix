5a0bb0a59719e750f3ea276de63d693430d4a622
Leonardo Anjoletto Ferreira <2048533-anjoletto@users.noreply.gitlab.com>
2025-02-06 14:32:06 -0300

    refac: somente python (por enquanto)

--- src/shell.nix
+++ src/shell.nix
@@ -3,14 +3,6 @@
 } }:
   pkgs.mkShell {
       nativeBuildInputs = with pkgs.buildPackages; [
-        clang-tools
-        clang
-        gcc
-        cmake
-        python314
-        jetbrains.idea-ultimate
-        jetbrains.jdk
-        mpi
-        grpc
+        python312
     ];
   }
